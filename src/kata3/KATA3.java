3eccf4260d87c51f615a23497159a3e46eced9c7
usuario <usuario@10.22.143.54>
2016-10-21 11:33:37 +0100

    Kata 3 (intento 1)

---
+++ src/kata3/KATA3.java
@@ -0,0 +1,12 @@
+
+package kata3;
+
+
+public class KATA3 {
+
+
+    public static void main(String[] args) {
+       HistogramDisplay histo = new HistogramDisplay();
+       histo.execute();
+    }
+}
